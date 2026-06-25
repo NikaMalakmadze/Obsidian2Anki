0cc6dcc35288d6a2cff3f42c14869434a4373fc6
nikamalakmadze <malakmadzenika0909@gmail.com>
2026-06-25 23:50:10 +0400

    Working On Main Script Logic

--- src/obsidian2anki/main.py
+++ src/obsidian2anki/main.py
@@ -2,22 +2,26 @@ from obsidian2anki.vault_manager import VaultManager
 from obsidian2anki.anki_manager import AnkiManager
 from obsidian2anki.ai import AI
 
+from obsidian2anki.config import get_settings, Settings
+
+settings: Settings = get_settings()
+
 v = VaultManager()
 ai = AI()
 am = AnkiManager()
 
-notes = v.process_files()
+notes = v._process_files("00_Inbox")
 
 print(notes)
 
 names: list[str] = [note.title for note in notes]
 
-cards = []
-for note in notes:
-    cards.extend(ai.generate_note_cards(note))
+# cards = []
+# for note in notes:
+#     cards.extend(ai.generate_note_cards(note))
 
-print(cards)
+# print(cards)
 
-ids = am.add_cards(cards)
+# ids = am.add_cards(cards)
 
-print(ids)
+# print(ids)
