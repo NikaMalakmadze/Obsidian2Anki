788548d4b7bab83ab2b1838461c429b1965b43fe
nikamalakmadze <malakmadzenika0909@gmail.com>
2026-07-02 23:26:34 +0400

    Working On Main Script Logic

--- src/obsidian2anki/main.py
+++ src/obsidian2anki/main.py
@@ -1,9 +1,10 @@
 import sys
 
 from obsidian2anki.core.vault_manager import VaultManager
+from obsidian2anki.core.state_manager import StateManager
 from obsidian2anki.core.anki_manager import AnkiManager
 from obsidian2anki.config import get_settings, Settings
-from obsidian2anki.models import Flashcard
+from obsidian2anki.models import Flashcard, NoteInfo
 from obsidian2anki.core.ai import AI
 
 
@@ -12,6 +13,7 @@ settings: Settings = get_settings()
 
 class Obsidian2Anki:
     def __init__(self) -> None:
+        self._state: StateManager = StateManager()
         self._vault: VaultManager = VaultManager()
         self._anki: AnkiManager = AnkiManager()
         self._ai: AI = AI()
@@ -23,9 +25,16 @@ class Obsidian2Anki:
             print("No Notes Found")
 
         for note in notes:
+            if self._state.in_state(note.title, note.content):
+                continue
+
             flash_cards: list[Flashcard] = self._ai.generate_note_cards(note)
             ids: list[int] = self._anki.add_cards(flash_cards)
             self._vault.write_metadata(note, ids)
+            self._state.update_state(NoteInfo(vault_info=note, card_ids=ids))
+
+        if self._state.is_changed():
+            self._state.set_state()
 
     def delete_card(self, card_id: str):
         try:
