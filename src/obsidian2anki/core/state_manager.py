41f9c100dbc55c64f6caee2a393e824544533c0e
nikamalakmadze <malakmadzenika0909@gmail.com>
2026-07-06 23:46:40 +0400

    Added State Loading And Anki Card Deleting Logic

--- src/obsidian2anki/core/state_manager.py
+++ src/obsidian2anki/core/state_manager.py
@@ -15,9 +15,8 @@ class StateManager:
         self.state_folder: Path = BASE_DIR / settings.STATE_FOLDER
         self.state_file: Path = self.state_folder / "state.json"
         self._temp_data: list[tuple[str, StateNote]] = []
-
-    def is_changed(self) -> bool:
-        return len(self._temp_data) > 0
+        self._is_changed: bool = False
+        self._load_state()
 
     def in_state(self, note_title: str, note_content: str) -> bool:
         content_hash: str = sha256(note_content.encode("utf-8")).hexdigest()
@@ -26,7 +25,7 @@ class StateManager:
             and content_hash == self._state[note_title].content_hash
         )
 
-    def update_state(self, note: NoteInfo) -> None:
+    def prepare_for_state(self, note: NoteInfo) -> None:
         content_hash: str = sha256(note.vault_info.content.encode("utf-8")).hexdigest()
 
         curr_datetime: datetime = datetime.now(timezone.utc)
@@ -42,27 +41,46 @@ class StateManager:
 
         self._temp_data.append((note.vault_info.title, state_note))
 
+        self._is_changed = True
+
+    def delete_card(self, card_id: int) -> bool:
+        found: bool = False
+        for note_info in self._state.values():
+            if card_id not in note_info.anki_note_ids:
+                continue
+
+            curr_datetime: str = datetime.now(timezone.utc).isoformat()
+            note_info.updated_at = curr_datetime
+            note_info.anki_note_ids.remove(card_id)
+            found = True
+
+        self._is_changed = True
+        self.set_state()
+        return found
+
     def set_state(self) -> None:
-        for k, v in self._temp_data:
-            self._state[k] = v
+        if not self._is_changed:
+            return
+
+        self._extend_state()
 
         self.state_file.write_text(
-            json.dumps({k: v.model_dump() for k, v in self._state.items()}, indent=4)
+            json.dumps(
+                {k: v.model_dump() for k, v in self._state.items()},
+                indent=4,
+                ensure_ascii=False,
+            ),
+            encoding="utf-8",
         )
 
         self._temp_data = []
+        self._is_changed = False
 
+    def _extend_state(self) -> None:
+        for k, v in self._temp_data:
+            self._state[k] = v
 
-StateManager().set_state(
-    [
-        NoteInfo(
-            vault_info={
-                "title": "Python Notes",
-                "tags": ["python", "pydantic"],
-                "content": "Some note content",
-                "path": "/notes/python.md",
-            },
-            card_ids=[1, 2, 3],
-        )
-    ]
-)
+    def _load_state(self) -> None:
+        plain_text: str = self.state_file.read_text(encoding="utf-8")
+        plain_json: dict = json.loads(plain_text) if plain_text else {}
+        self._state = {k: StateNote.model_validate(v) for k, v in plain_json.items()}
