7f7be929e254e3a5f759293cfc424a5d1c782449
nikamalakmadze <malakmadzenika0909@gmail.com>
2026-07-12 23:52:17 +0400

    Created Logger

---
+++ src/obsidian2anki/utils/logger.py
@@ -0,0 +1,23 @@
+from logging.handlers import RotatingFileHandler
+import logging
+
+from obsidian2anki.config import BASE_DIR
+
+LOG_DIR = BASE_DIR / "logs"
+LOG_DIR.mkdir(exist_ok=True)
+LOG_FILE = LOG_DIR / "obsidian2anki.log"
+
+file_handler = RotatingFileHandler(
+    LOG_FILE, maxBytes=5 * 1024 * 1024, backupCount=3, encoding="utf-8"
+)
+
+
+def setup_logger(level=logging.INFO):
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        handlers=[
+            file_handler,
+            logging.StreamHandler(),
+        ],
+    )
