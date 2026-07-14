e48d878a273644bee1fe4fe3363f0d7e06830996
nikamalakmadze <malakmadzenika0909@gmail.com>
2026-07-14 22:26:19 +0400

    Small Changes

--- src/obsidian2anki/utils/type.py
+++ src/obsidian2anki/utils/type.py
@@ -1,7 +1,18 @@
 from pydantic import BaseModel, Field
 from typing import Literal, TypedDict
 
+
 Action = Literal["deckNames", "changeDeck", "addNotes", "deleteNotes", "getTags"]
+
+StateNoteProperties = Literal[
+    "title",
+    "path",
+    "content_hash",
+    "card_count",
+    "anki_note_ids",
+    "processed_at",
+    "updated_at",
+]
 
 
 class DeckParamsDict(TypedDict, total=False):
