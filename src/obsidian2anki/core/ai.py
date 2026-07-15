88207beb5dd64f6726bd0a0b94d1c2251967d365
nikamalakmadze <malakmadzenika0909@gmail.com>
2026-07-15 22:07:50 +0400

    Implemented Exponential BackOff Request Logic

--- src/obsidian2anki/core/ai.py
+++ src/obsidian2anki/core/ai.py
@@ -1,9 +1,14 @@
+from google.genai.errors import ClientError
 from pathlib import Path
 from google import genai
+import logging
+import time
 
 from obsidian2anki.models import FlashcardBatch, VaultNote, Flashcard
 from obsidian2anki.config import get_settings, Settings, BASE_DIR
 
+
+logger = logging.getLogger(__name__)
 settings: Settings = get_settings()
 
 
@@ -11,21 +16,30 @@ class AI:
     def __init__(self) -> None:
         self.client = genai.Client(api_key=settings.API_KEY)
         self.prompt: str = self._get_prompt()
+        self.delay: int = 7
 
     def generate_note_cards(self, note: VaultNote) -> list[Flashcard]:
-        response = self.client.models.generate_content(
-            model="gemini-3.1-flash-lite",
-            contents=self.prompt
-            + f"\n\n{note.model_dump_json(indent=2, exclude=['id', 'path'])}",
-            config={
-                "response_mime_type": "application/json",
-                "response_schema": FlashcardBatch,
-            },
-        )
+        while True:
+            try:
+                response = self.client.models.generate_content(
+                    model="gemini-3.1-flash-lite",
+                    contents=self.prompt
+                    + f"\n\n{note.model_dump_json(indent=2, exclude=['id', 'path'])}",
+                    config={
+                        "response_mime_type": "application/json",
+                        "response_schema": FlashcardBatch,
+                    },
+                )
+                break
+            except ClientError:
+                logger.warning("Rate limit hit. Retrying in %s seconds...", self.delay)
+                time.sleep(self.delay)
+                self.delay = min(self.delay * 2, 60)
 
         cards: FlashcardBatch = FlashcardBatch.model_validate_json(response.text)
         return cards.cards
 
-    def _get_prompt(self) -> str:
+    @staticmethod
+    def _get_prompt() -> str:
         prompt_file: Path = BASE_DIR / settings.PROMPT_FILE
         return prompt_file.read_text(encoding="utf-8")
