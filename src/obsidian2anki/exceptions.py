d80b433b445cac98adf6a65dfbec0dca3207eab2
nikamalakmadze <malakmadzenika0909@gmail.com>
2026-08-06 23:50:22 +0400

    Created Custom Exceptions For Anki

--- src/obsidian2anki/exceptions.py
+++ src/obsidian2anki/exceptions.py
@@ -6,5 +6,21 @@ class ObsidianFolderDoesNotExists(O2ABaseException):
     pass
 
 
-class AnkiValidationException(O2ABaseException):
-    pass
+class AnkiError(O2ABaseException):
+    """Base exception for Anki-related failures."""
+
+
+class AnkiConnectionError(AnkiError):
+    """AnkiConnect could not be reached."""
+
+
+class AnkiResponseError(AnkiError):
+    """AnkiConnect returned a malformed response."""
+
+
+class AnkiActionError(AnkiError):
+    """AnkiConnect executed the request but rejected the action."""
+
+
+class AnkiDuplicateNoteError(AnkiActionError):
+    """Anki rejected one or more notes as duplicates."""
